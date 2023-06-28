6ea246ecee301991b4278d90be40905175801671
kimyiseul <kimdew402@gmail.com>
2023-06-28 20:03:43 +0900

    2023.06.28 dd

---
+++ JS/index.js
@@ -0,0 +1,34 @@
+var slides = document.querySelector('.slides'),
+    slide = document.querySelectorAll('.slides li'),
+    currentIdx = 0,
+    slideCount = slide.length,
+    prevBtn = document.querySelector('.prev'),
+    slideWidth = 200,
+    slideMargin = 30,
+    nextBtn = document.querySelector('.next');
+
+    slides.style.width= (slideWidth+slideMargin)*slideCount-slideMargin+ 'px';
+
+    function moveSlide(num){
+        slides.style.left= -num*230+'px';
+        currentIdx = num;
+    }
+    nextBtn.addEventListener('click',function(){
+        if(currentIdx<slideCount-4){
+            moveSlide(currentIdx+1);
+        } else {
+            moveSlide(0);
+        }        
+    });
+
+    function moveSlide(num){
+        slides.style.left= num*230+'px';
+        currentIdx = num;
+    }
+    prevBtn.addEventListener('click',function(){
+        if(currentIdx>0){
+            moveSlide(currentIdx-1);
+        } else {
+            moveSlide(slideCount);
+        }        
+    });
